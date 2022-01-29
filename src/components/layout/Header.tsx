052ba24f0d6fcad033de85f37e889a2c33f9aa6d
jogabi <rkql1109@naver.com>
2022-01-29 16:08:06 +0900

    header 추가중

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
 
 const Header = () => (
-    <div>Header</div>
+  <div className="headerWrap">
+    <div className="log">
+      <Link href="/">
+        <a>SeeMe</a>
+      </Link>
+    </div>
+    <ul className="gnb">
+      <li>
+        <Link href="/">
+          <a>구직활동</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/">
+          <a>인재채용</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/">
+          <a>미디어 뉴스</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/">
+          <a>고객센터</a>
+        </Link>
+      </li>
+        </ul>
+        
+        
+  </div>
 );
 
 export default Header;
