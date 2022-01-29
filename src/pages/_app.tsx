121996098c858d7b1409ab4af8150fe61dc9e076
Avantgarde95 <mathematicianscott@gmail.com>
2022-01-29 14:56:47 +0900

    [feat] 공통 코드 제작
    * _app.tsx
      * 모든 페이지에서 공통으로 실행되는 코드
      * 공통 css나 page의 공통 wrapper 넣는 역할

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+/**
+ * 모든 페이지에서 돌아가는 코드.
+ */
+
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+/**
+ * 임의의 page의 wrapper.
+ */
+const App = ({ Component, pageProps }: AppProps) => (
+    <>
+        <Head>
+            <title>천오</title>
+        </Head>
+        {/* Component에 페이지가 들어감. */}
+        <Component {...pageProps} />
+    </>
+);
+
+export default App;
