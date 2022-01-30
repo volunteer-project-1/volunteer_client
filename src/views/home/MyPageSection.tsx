0a2dd6e11a2268993a556cf64d0a0ed4a29f6bf2
Avantgarde95 <mathematicianscott@gmail.com>
2022-01-30 17:24:34 +0900

    [chore] Section component를 MyPageSection에도 적용
    * MyPageSection에도 공통 component 사용하여 제목 및 설명 붙임

--- src/views/home/MyPageSection.tsx
+++ src/views/home/MyPageSection.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+
+import Section from '@/components/home/Section';
 import styles from "./SampleResponse.module.scss";
+
 const MyPageSection = () => (
-  <div className={styles.body}>MyPageSectionsdofso 11111</div>
+    <Section
+        titleParts={[
+            '자기 PR도',
+            '영상으로'
+        ]}
+        descriptionParts={[
+            '영상으로 이력서를 남겨 사회에 나를 소개해 보아요.'
+        ]}
+    >
+        <div className={styles.body}>(영상)</div>
+    </Section>
 );
 
 export default MyPageSection;
