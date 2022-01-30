2f7c09caad9dc0f84af0441c6d470008f303a87d
jogabi <rkql1109@naver.com>
2022-01-30 10:55:49 +0900

    css utils 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,11 @@
  * 모든 페이지에서 돌아가는 코드.
  */
 
-import { AppProps } from 'next/app';
+import {
+    AppProps
+}
+
+from 'next/app';
 import Head from 'next/head';
 
 import '@/assets/scss/reset.scss'
@@ -10,14 +14,18 @@ import '@/assets/scss/reset.scss'
 /**
  * 임의의 page의 wrapper.
  */
-const App = ({ Component, pageProps }: AppProps) => (
-    <>
-        <Head>
-            <title>천오</title>
-        </Head>
-        {/* Component에 페이지가 들어감. */}
-        <Component {...pageProps} />
-    </>
-);
+const App=( {
+        Component, pageProps
+    }
+
+    : AppProps)=>(<> <Head> <title>천오</title> </Head> {
+        /* Component에 페이지가 들어감. */
+    }
+
+    <Component {
+        ...pageProps
+    }
+
+    /> </>);
 
 export default App;
