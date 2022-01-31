aedbb5a355cf05f61b383c41fd6b85dc90ee52be
jogabi <rkql1109@naver.com>
2022-01-31 17:26:48 +0900

    header,footer layout css 삭제 * classname style 붙여주는작업 효율성 떨어짐

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,35 +1,36 @@
 import React from "react";
 import Link from "next/link";
-import style from "./layout.module.scss";
 const menus = ["구직활동", "인재채용", "미디어 뉴스", "고객센터"];
 
 const Header = () => (
-  <div className={style.headerWrap}>
-    <div className={style.logo}>
+  <div className="headerWrap">
+    <div className="logo">
       <Link href="/">
         <a>SeeMe</a>
       </Link>
     </div>
-    <div className={style.cs_login}>
+    <div className="cs_login">
       <Link href="/">
-        <a className={style.lg}>로그인</a>
+        <a className="lg">로그인</a>
       </Link>
       <Link href="/">
         <a>회원가입</a>
       </Link>
     </div>
 
-    <ul className={style.gnb}>
-      {menus.map((menu, index) => {
-        return (
-          <li key={index}>
-            <Link href="/">
-              <a>{menu}</a>
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
+    <div className="header_scroll">
+      <ul className="gnb">
+        {menus.map((menu, index) => {
+          return (
+            <li key={index}>
+              <Link href="/">
+                <a>{menu}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   </div>
 );
 
