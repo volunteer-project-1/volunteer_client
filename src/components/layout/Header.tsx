94e777133d9cf31e6f08282111427cf50b71d5fa
jogabi <rkql1109@naver.com>
2022-01-31 17:36:59 +0900

    header css 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,18 +4,20 @@ const menus = ["구직활동", "인재채용", "미디어 뉴스", "고객센터
 
 const Header = () => (
   <div className="headerWrap">
-    <div className="logo">
-      <Link href="/">
-        <a>SeeMe</a>
-      </Link>
-    </div>
-    <div className="cs_login">
-      <Link href="/">
-        <a className="lg">로그인</a>
-      </Link>
-      <Link href="/">
-        <a>회원가입</a>
-      </Link>
+    <div className="header_top">
+      <div className="logo">
+        <Link href="/">
+          <a>SeeMe</a>
+        </Link>
+      </div>
+      <div className="cs_login">
+        <Link href="/">
+          <a className="lg">로그인</a>
+        </Link>
+        <Link href="/">
+          <a>회원가입</a>
+        </Link>
+      </div>
     </div>
 
     <div className="header_scroll">
