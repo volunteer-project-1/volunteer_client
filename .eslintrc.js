0a1028fd86e80d669791a14920ade533f0d8ff78
Avantgarde95 <mathematicianscott@gmail.com>
2022-02-13 15:02:43 +0900

    [feat] ESLint + prettier 이용한 코드 스타일 체크
    * 코드 스타일 통일을 위해 npm run lint 시에 들여쓰기, 세미콜론 등을 검사하도록 함
      * 들여쓰기: 타 개발자의 스타일과 맞춰 2칸으로 조정
      * 줄 끝(LF/CRLF): 어차피 git 쪽에서 커밋 시 맞춰주므로 체크 시에는
        어느  한쪽으로 고정하지 않음
      * 나머지: 적당히 설정함

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'plugin:prettier/recommended'
+  ],
+  // 체크에서 제외.
+  ignorePatterns: [
+    'node_modules/'
+  ],
+  rules: {
+    'prettier/prettier': ['error', {
+      // "..." 대신 '...' 사용.
+      singleQuote: true,
+      // ; 필수.
+      semi: true,
+      // 탭 대신 스페이스 사용.
+      useTabs: false,
+      // 들여쓰기 단위.
+      tabWidth: 2,
+      // 최대 너비.
+      printWidth: 120,
+      // 화살표 함수에서 인수가 1개면 괄호 X.
+      arrowParens: 'avoid',
+      // 줄 끝(LF/CRLF): 각 파일에 따라 알아서.
+      endOfLine: 'auto'
+    }]
+  }
+};
