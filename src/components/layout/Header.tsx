07b91f8780d4b20dd8a126d3dea29f796d0023d8
jogabi <rkql1109@naver.com>
2022-02-15 20:29:31 +0900

    반응형 vw vh function 추가 header 재작업중

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,34 +8,36 @@ const menus = ["구직활동", "인재채용", "미디어 뉴스", "고객센터
 
 const Header = () => (
   <div className="headerWrap">
-    <div className="header_top">
-      <div className="logo">
-        <Link href="/">
-          <a>
-            <Image src={Logo} alt="SeeMe" />
-          </a>
-        </Link>
+    <div className="inner">
+      <div className="header_top">
+        <div className="logo">
+          <Link href="/">
+            <a>
+              <Image src={Logo} alt="SeeMe" />
+            </a>
+          </Link>
+        </div>
+        <div className="cs_login">
+          <Link href="/">
+            <a className="lg">로그인</a>
+          </Link>
+          <Link href="/">
+            <a>회원가입</a>
+          </Link>
+        </div>
       </div>
-      <div className="cs_login">
-        <Link href="/">
-          <a className="lg">로그인</a>
-        </Link>
-        <Link href="/">
-          <a>회원가입</a>
-        </Link>
-      </div>
-    </div>
 
-    <div className="header_scroll">
-      <ul className="gnb">
-        {menus.map((menu, index) => (
-          <li key={index}>
-            <Link href="/">
-              <a>{menu}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <div className="header_scroll">
+        <ul className="gnb">
+          {menus.map((menu, index) => (
+            <li key={index}>
+              <Link href="/">
+                <a>{menu}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   </div>
 );
