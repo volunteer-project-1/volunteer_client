7193a888ae7822a7aac27c8f06c892e134deab45
Avantgarde95 <mathematicianscott@gmail.com>
2022-02-16 06:43:32 +0900

    [feat] ContactSection 보내기 버튼 추가 / 입력 구현
    * ContactSection
      * 보내기 버튼 추가
      * 입력 구현
        * 입력값 및 체크값들을 state에 저장
        * 보내기 버튼 누르면 테스트용으로 현재 state를 alert로 띄워줌

--- src/views/home/ContactSection.tsx
+++ src/views/home/ContactSection.tsx
@@ -1,34 +1,116 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 
 import styles from "@/views/home/ContactSection.module.scss";
 import Background from "@/images/home-contact-background.jpg";
 
-const ContactSection = () => (
-  <div className={styles.contactSection}>
-    <img className={styles.background} src={Background.src} alt={"Contact"} />
-    <div className={styles.content}>
-      <div className={styles.aboutRow}>
-        <div>
-          <div className={styles.aboutTitle}>Contact us!</div>
-          <div className={styles.aboutDescription}>
-            언제든 궁금한 점이 있으면 연락주세요.
-            <br />
-            언제나 답변해드립니다.
+/**
+ * 사용자가 입력한 것들.
+ */
+interface Inputs {
+  // 이름.
+  name: string;
+  // 연락처.
+  contact: string;
+  // 이메일.
+  email: string;
+  // 문의사항.
+  question: string;
+}
+
+/**
+ * 사용자가 체크한 것들.
+ */
+interface Checks {
+  // 개인정보 취급방침.
+  privacy: boolean;
+}
+
+const ContactSection = () => {
+  const [inputs, setInputs] = useState<Inputs>({ name: "", contact: "", email: "", question: "" });
+  const [checks, setChecks] = useState<Checks>({ privacy: false });
+
+  const onChangeInput = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, key: keyof Inputs) => {
+    setInputs({ ...inputs, [key]: event.target.value });
+  };
+
+  const onChangeCheck = (event: ChangeEvent<HTMLInputElement>, key: keyof Checks) => {
+    setChecks({ ...checks, [key]: event.target.checked });
+  };
+
+  const onClickSend = () => {
+    alert(JSON.stringify({ inputs, checks }, null, 2));
+  };
+
+  return (
+    <div className={styles.contactSection}>
+      <img className={styles.background} src={Background.src} alt={"Contact"} />
+      <div className={styles.content}>
+        <div className={styles.aboutRow}>
+          <div>
+            <div className={styles.aboutTitle}>Contact us!</div>
+            <div className={styles.aboutDescription}>
+              언제든 궁금한 점이 있으면 연락주세요.
+              <br />
+              언제나 답변해드립니다.
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.formRow}>
-        <div className={styles.formTitle}>사용자 정보</div>
-        <input className={styles.formTextField} type={"text"} placeholder={"이름"} />
-        <input className={styles.formTextField} type={"text"} placeholder={"연락처"} />
-        <input className={styles.formTextField} type={"text"} placeholder={"메일주소"} />
-        <textarea className={styles.formTextArea} rows={5} placeholder={"문의사항을 작성해주세요."} />
-        <div>
-          <input className={styles.formCheckBox} type={"checkbox"} />
+        <div className={styles.formRow}>
+          <div className={styles.formTitle}>사용자 정보</div>
+          <input
+            className={styles.formTextField}
+            type={"text"}
+            placeholder={"이름"}
+            value={inputs.name}
+            onChange={event => {
+              onChangeInput(event, "name");
+            }}
+          />
+          <input
+            className={styles.formTextField}
+            type={"text"}
+            placeholder={"연락처"}
+            value={inputs.contact}
+            onChange={event => {
+              onChangeInput(event, "contact");
+            }}
+          />
+          <input
+            className={styles.formTextField}
+            type={"email"}
+            placeholder={"메일주소"}
+            value={inputs.email}
+            onChange={event => {
+              onChangeInput(event, "email");
+            }}
+          />
+          <textarea
+            className={styles.formTextArea}
+            rows={5}
+            placeholder={"문의사항을 작성해주세요."}
+            value={inputs.question}
+            onChange={event => {
+              onChangeInput(event, "question");
+            }}
+          />
+          <label className={styles.formLabel}>
+            <input
+              className={styles.formCheckBox}
+              type={"checkbox"}
+              checked={checks.privacy}
+              onChange={event => {
+                onChangeCheck(event, "privacy");
+              }}
+            />
+            개인정보 취급방침 동의
+          </label>
+          <button className={styles.formButton} type={"button"} onClick={onClickSend}>
+            보내기
+          </button>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ContactSection;
