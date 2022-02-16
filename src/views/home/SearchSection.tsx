21d51d0c452a09bc486e105158a4963b2eeadfae
Avantgarde95 <mathematicianscott@gmail.com>
2022-02-16 15:34:33 +0900

    [feat] SearchSection 아이콘 추가 및 태그 버튼 추가
    * 아이콘 추가
      * 검색 입력칸 안쪽에 돋보기 모양 아이콘 넣음
    * 태그 버튼 추가
      * 아직은 동작 정의가 없으므로 그냥 클릭 시 입력값 뒤에 태그를 붙이도록 함

--- src/views/home/SearchSection.tsx
+++ src/views/home/SearchSection.tsx
@@ -1,7 +1,11 @@
 import React, { ChangeEvent, useState } from "react";
 
 import styles from "@/views/home/SearchSection.module.scss";
-import Background from "@/images/home-search.jpg";
+import Background from "@/images/home-search-background.jpg";
+import Icon from "@/images/home-search-icon.svg";
+
+// 임시로 만듬...
+const exampleTags: Array<string> = ["일자리", "패럴림픽", "공개채용", "장애인_일자리"];
 
 const SearchSection = () => {
   const [input, setInput] = useState("");
@@ -14,16 +18,36 @@ const SearchSection = () => {
     alert(`검색: "${input}"`);
   };
 
+  const onClickTag = (tag: string) => {
+    // 테스트용으로 그냥 입력값 뒤쪽에 클릭한 태그를 추가하도록 함.
+    setInput(`${input} ${tag}`);
+  };
+
   return (
     <div className={styles.searchSection}>
       <img className={styles.background} src={Background.src} alt={"Background"} />
       <div className={styles.content}>
         <div className={styles.description}>궁금한것은 무엇이든 검색해보세요!</div>
         <div className={styles.searchArea}>
+          <img className={styles.searchIcon} src={Icon.src} alt={"Icon"} />
           <input className={styles.searchInput} type={"text"} value={input} onChange={onChangeInput} />
           <button className={styles.searchButton} onClick={onClickSearch}>
             검색
           </button>
+        </div>
+        <div className={styles.tags}>
+          {exampleTags.map(tag => (
+            <button
+              className={styles.tagButton}
+              key={tag}
+              type={"button"}
+              onClick={() => {
+                onClickTag(tag);
+              }}
+            >
+              #{tag}
+            </button>
+          ))}
         </div>
       </div>
     </div>
