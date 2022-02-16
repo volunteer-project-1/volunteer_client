7ba02d5f6830c52fe13f27a2b96bada48e9c6d23
Avantgarde95 <mathematicianscott@gmail.com>
2022-02-16 11:17:08 +0900

    [feat] SearchSection 배경 및 내용 추가
    * SearchSection
      * 배경 이미지 추가
      * 설명 문구 추가
      * 검색칸 및 검색버튼 추가
        * 검색버튼 누르면 그냥 검색칸 내용 alert로 띄움
      * (해시태그는 아직 추가 안 함)

--- src/views/home/SearchSection.tsx
+++ src/views/home/SearchSection.tsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 
-const SearchSection = () => <div>궁금한 것은 무엇이든 검색해보세요</div>;
+import styles from "@/views/home/SearchSection.module.scss";
+import Background from "@/images/home-search.jpg";
+
+const SearchSection = () => {
+  const [input, setInput] = useState("");
+
+  const onChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
+
+  const onClickSearch = () => {
+    alert(`검색: "${input}"`);
+  };
+
+  return (
+    <div className={styles.searchSection}>
+      <img className={styles.background} src={Background.src} alt={"Background"} />
+      <div className={styles.content}>
+        <div className={styles.description}>궁금한것은 무엇이든 검색해보세요!</div>
+        <div className={styles.searchArea}>
+          <input className={styles.searchInput} type={"text"} value={input} onChange={onChangeInput} />
+          <button className={styles.searchButton} onClick={onClickSearch}>
+            검색
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SearchSection;
