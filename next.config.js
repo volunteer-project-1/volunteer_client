866dc69200f1b706ce9147ee7823944c12ad9fd5
Avantgarde95 <mathematicianscott@gmail.com>
2022-02-22 04:47:55 +0900

    [bugfix] sass(scss) 나누기 연산자 경고 처리
    * _utils.scss에서 나누기 연산자(`/`) 써서 경고 뜸
      * 경고 메시지 따라서 math.div로 변경해서 해결함
    * 기타
      * next.config.js 안 쓰이는 옵션 제거
      * package.json 들여쓰기 2칸으로 변경

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    sassOptions: {
-        // SCSS 파일들에 자동으로 넣을 코드.
-        // prependData 옵션이랑 동일. (additionalData가 더 최신 이름인듯)
-        additionalData: `@use "@/assets/scss/index" as *;`,
-        productionBrowserSourceMaps: true,
-    }
+  sassOptions: {
+    // SCSS 파일들에 자동으로 넣을 코드.
+    // prependData 옵션이랑 동일. (additionalData가 더 최신 이름인듯)
+    additionalData: `@use "@/assets/scss/index" as *;`
+  }
 };
