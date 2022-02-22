3530b048433a97ff12e68e1bd3bdb9e54e3b0257
Avantgarde95 <mathematicianscott@gmail.com>
2022-02-23 08:08:46 +0900

    [feat] MainSection에 설명 부분 추가
    * MainSection
      * 사진 부분 오른쪽의 초록색 반투명 사각형 부분 추가
      * (스타일링은 미완)
    * common.scss의 boxColor(): opacity 설정할 수 있도록 함

--- src/views/home/MainSection.tsx
+++ src/views/home/MainSection.tsx
@@ -5,7 +5,19 @@ import Background1 from "@/images/home/main-background1.jpg";
 
 const MainSection = () => (
   <div className={styles.mainSection}>
-    <img className={styles.background} src={Background1.src} alt={"Background"} />
+    <div className={styles.content}>
+      <img className={styles.background} src={Background1.src} alt={"Background"} />
+      <div className={styles.textArea}>
+        <div className={styles.title1}>세상 밖으로 나오는 당신의 도전</div>
+        <div className={styles.title2}>당신의 능력을 보여주세요</div>
+        <div className={styles.description}>
+          세상과 소통하고 싶은 당신을 위해 준비했습니다.
+          <br />
+          세상밖으로 나와 당신의 능력을 마음껏 펼쳐보아요! &apos;See Me&apos; 에서 여러분을 인도해 드릴 것입니다. 당신의
+          능력을 보여주세요. &apos;See Me&apos; 는 당신을 기다리고 있습니다.
+        </div>
+      </div>
+    </div>
   </div>
 );
 
