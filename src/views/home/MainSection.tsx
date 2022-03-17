acf802c465ba24fd605b6435bf62f2c7e65a71e2
Avantgarde95 <mathematicianscott@gmail.com>
2022-03-18 08:12:54 +0900

    [chore] MainSection 및 TitledSection 스타일 수정 / 자료형 체크 더 엄격하게
    * MainSection 및 TitledSection: 가이드 변경에 맞춰서 스타일 수정
    * tsconfig.json: 안전을 위해... strict 옵션을 true로 바꿔서 null, undefined 체크 등을 더 엄격하게 하도록 변경

--- src/views/home/MainSection.tsx
+++ src/views/home/MainSection.tsx
@@ -25,9 +25,16 @@ const MainSection = () => (
         <div className={styles.description}>
           세상과 소통하고 싶은 당신을 위해 준비했습니다.
           <br />
-          세상밖으로 나와 당신의 능력을 마음껏 펼쳐보아요! &apos;See Me&apos; 에서 여러분을 인도해 드릴 것입니다. 당신의
-          능력을 보여주세요. &apos;See Me&apos; 는 당신을 기다리고 있습니다.
+          세상밖으로 나와 당신의 능력을 마음껏 펼쳐보아요!
+          <br />
+          <br />
+          &apos;See Me&apos; 에서 여러분을 인도해 드릴 것입니다.
+          <br />
+          &apos;See Me&apos; 는 당신을 기다리고 있습니다.
         </div>
+        <button className={styles.moreButton} type={"button"}>
+          Learn More
+        </button>
       </div>
     </div>
   </div>
