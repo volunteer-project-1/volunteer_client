246f557359cece3497af8db050d5c488dffe81a9
Avantgarde95 <mathematicianscott@gmail.com>
2022-04-06 21:17:29 +0900

    [bugfix] 개발 모드에서 가끔 스타일 안 먹는 버그 수정
    * 개발 모드에서는 웹페이지를 너무 빨리 열면 스타일 안 먹는 경우가 있음...
      * 임시적인 fix: 개발 모드에서는 페이지를 무조건 CSR(client-side rendering)을 함
      * SSR 중인지 CSR 중인지 판단은 useEffect() 이용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from "react";
 import { AppProps } from "next/app";
 import { CacheProvider, EmotionCache } from "@emotion/react";
 import { ThemeProvider } from "@mui/material";
 
 import { storeWrapper } from "@/store";
+import { isDevelopmentMode } from "@/utils/DebugUtils";
 import { createEmotionCache, muiLightTheme } from "@/utils/StyleUtils";
 import "@/scss/reset.scss";
 
@@ -15,13 +17,26 @@ interface MyAppProps extends AppProps {
 /**
  * 임의의 page에 씌워지는 wrapper component.
  */
-const MyApp = ({ Component, emotionCache = clientSideEmotionCache, pageProps }: MyAppProps) => (
-  <CacheProvider value={emotionCache}>
-    <ThemeProvider theme={muiLightTheme}>
-      {/* <Component/>에 pages 폴더에 있는 것들이 들어감. */}
-      <Component {...pageProps} />
-    </ThemeProvider>
-  </CacheProvider>
-);
+const MyApp = ({ Component, emotionCache = clientSideEmotionCache, pageProps }: MyAppProps) => {
+  // SSR 중이면 false, CSR 중이면 true.
+  const [isClient, setClient] = useState(false);
+
+  // 개발 모드에서는 웹페이지를 너무 빨리 열면 스타일 안 먹는 경우가 있음...
+  // 임시적인 fix: 개발 모드에서는 페이지를 무조건 CSR(client-side rendering)을 함.
+  const renderPage = !isDevelopmentMode() || isClient;
+
+  useEffect(() => {
+    setClient(true);
+  }, []);
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={muiLightTheme}>
+        {/* <Component/>에 pages 폴더에 있는 것들이 들어감. */}
+        {renderPage && <Component {...pageProps} />}
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
 
 export default storeWrapper.withRedux(MyApp);
