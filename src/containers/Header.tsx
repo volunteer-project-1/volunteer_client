6465ef376221dd3199ddd8f76d0715816aa685bf
Avantgarde95 <mathematicianscott@gmail.com>
2022-05-11 16:45:48 +0900

    [feat] 헤더에 드롭다운 추가
    * 헤더에 드롭다운 추가
      * 각 상세 페이지 탐색에 필요
      * 현재 정해진 UI가 없으므로 일단 MUI Menu 이용해서 추가

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import classNames from "classnames";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 import ROUTES from "@/constants/Routes";
 import { useStoreSelector } from "@/store";
@@ -9,22 +11,39 @@ import "@/containers/Header.scoped.scss";
 
 interface Menu {
   name: string;
-  url: string;
   // 현재 메뉴가 어느 route에 해당하는지 판단하기 위해 사용.
   // TODO: 하드코딩 대신 ROUTES 객체 활용하도록 변경.
   prefix: string;
+  pages: Array<{ name: string; url: string }>;
 }
 
 const menus: Array<Menu> = [
-  { name: "구직활동", url: ROUTES.seeker.resumeEditor, prefix: "/seeker" },
-  { name: "인재채용", url: ROUTES.company.seekerList, prefix: "/company" },
-  { name: "미디어 뉴스", url: ROUTES.news.newsList, prefix: "/news" },
-  { name: "고객센터", url: ROUTES.notice, prefix: "/notice" },
+  {
+    name: "구직활동",
+    prefix: "/seeker",
+    pages: [
+      { name: "구직자 프로필 작성", url: ROUTES.seeker.resumeEditor },
+      { name: "기업 리스트", url: ROUTES.seeker.companyList },
+    ],
+  },
+  {
+    name: "인재채용",
+    prefix: "/company",
+    pages: [
+      { name: "기업 프로필 작성", url: ROUTES.company.infoEditor },
+      { name: "구직자 리스트", url: ROUTES.company.seekerList },
+    ],
+  },
+  { name: "미디어 뉴스", prefix: "/news", pages: [{ name: "미디어 뉴스 리스트", url: ROUTES.news.newsList }] },
+  { name: "고객센터", prefix: "/notice", pages: [{ name: "고객센터", url: ROUTES.notice }] },
 ];
 
 const Header = () => {
   const session = useStoreSelector(state => state.auth.session);
   const router = useRouter();
+
+  const menusRef = useRef<Array<HTMLLIElement | null>>(menus.map(() => null));
+  const [selectedMenuIndex, setSelectedMenuIndex] = useState<number | null>(null);
 
   // For test...
   const loginText = session === null ? "LOG IN" : session.type;
@@ -59,16 +78,52 @@ const Header = () => {
 
         <div className="header_scroll">
           <ul className="gnb">
-            {menus.map(({ name, url, prefix }) => (
+            {menus.map((menu, index) => (
               <li
                 className={classNames("menu", {
-                  isActive: router.pathname.startsWith(prefix),
+                  isActive: router.pathname.startsWith(menu.prefix),
                 })}
-                key={name}
+                key={menu.name}
+                ref={element => {
+                  menusRef.current[index] = element;
+                }}
               >
-                <Link href={url}>
-                  <a>{name}</a>
-                </Link>
+                <button
+                  className="menu_name"
+                  type="button"
+                  onClick={() => {
+                    if (index === selectedMenuIndex) {
+                      setSelectedMenuIndex(null);
+                    } else {
+                      setSelectedMenuIndex(index);
+                    }
+                  }}
+                >
+                  {menu.name}
+                </button>
+                <Menu
+                  open={index === selectedMenuIndex}
+                  onClose={() => {
+                    setSelectedMenuIndex(null);
+                  }}
+                  anchorEl={menusRef.current[index]}
+                  // Menu 열었을 때 스크롤바 사라지는 현상 방지.
+                  disableScrollLock
+                >
+                  {menu.pages.map(page => (
+                    <MenuItem
+                      sx={{
+                        minWidth: menusRef.current[index]?.clientWidth,
+                      }}
+                      key={page.name}
+                      onClick={() => {
+                        router.push(page.url);
+                      }}
+                    >
+                      {page.name}
+                    </MenuItem>
+                  ))}
+                </Menu>
               </li>
             ))}
           </ul>
