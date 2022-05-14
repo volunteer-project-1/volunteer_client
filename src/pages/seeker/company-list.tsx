5c213c6c847227e937b5fac934e736b70da6deaf
Avantgarde95 <mathematicianscott@gmail.com>
2022-05-14 21:49:35 +0900

    [feat] 기업 리스트 페이지 일부 구현
    * 기업 카드 리스트 일부 구현
      * 카드 width 조정 필요
      * react-slick 비슷한거 사용 필요
    * 기업 검색 결과 일부 구현
      * 정보 추가 필요

--- src/pages/seeker/company-list.tsx
+++ src/pages/seeker/company-list.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import Link from "next/link";
 
 import Page from "@/components/page";
+import CompanySection from "@/containers/seeker/company-list/CompanySection";
+import ResultSection from "@/containers/seeker/company-list/ResultSection";
 
 /**
  * 기업 리스트 페이지.
@@ -8,6 +11,19 @@ import Page from "@/components/page";
 const CompanyListPage = () => (
   <Page>
     <Page.Title>인기있는 채용공고</Page.Title>
+    <CompanySection />
+    <Page.Columns>
+      <Page.Column>
+        <Link href="https://www.kead.or.kr/view/service/service04_02_01.jsp?sub2=1">
+          <a>
+            <img src="/assets/company/banner.svg" alt="배너" />
+          </a>
+        </Link>
+      </Page.Column>
+      <Page.Column fill>
+        <ResultSection />
+      </Page.Column>
+    </Page.Columns>
   </Page>
 );
 
