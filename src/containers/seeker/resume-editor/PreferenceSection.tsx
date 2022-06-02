f588a5af55e7f933a4b696e5a203ffefc224a338
Avantgarde95 <mathematicianscott@gmail.com>
2022-06-02 20:44:48 +0900

    [feat] 이력서 공개/비공개 여부 설정
    * 이력서 공개/비공개 여부 설정하는 UI 추가

--- src/containers/seeker/resume-editor/PreferenceSection.tsx
+++ src/containers/seeker/resume-editor/PreferenceSection.tsx
@@ -8,6 +8,7 @@ import {
   updatePreferenceLocation,
   updatePreferenceJob,
   updatePreference,
+  updateResume,
 } from "@/store/resume";
 import Editor from "@/components/editor";
 
@@ -15,6 +16,7 @@ const PreferenceSection = () => {
   const preference = useStoreSelector(state => state.resume.preference);
   const preferenceJobs = useStoreSelector(state => state.resume.preferenceJobs);
   const preferenceLocations = useStoreSelector(state => state.resume.preferenceLocations);
+  const resume = useStoreSelector(state => state.resume.resume);
   const dispatch = useStoreDispatch();
 
   return (
@@ -81,6 +83,30 @@ const PreferenceSection = () => {
             dispatch(addPreferenceJob());
           }}
         />
+      </Editor.Row>
+      <Editor.Row>
+        <Editor.Cell fill>
+          <Editor.Checkbox
+            value={resume.is_public === true}
+            onChange={value => {
+              dispatch(updateResume({ is_public: true }));
+            }}
+          >
+            이력서를 모두 공개합니다.(로그인하지 않아도 이력서를 열람할수 있습니다.)
+          </Editor.Checkbox>
+        </Editor.Cell>
+      </Editor.Row>
+      <Editor.Row>
+        <Editor.Cell fill>
+          <Editor.Checkbox
+            value={resume.is_public !== true}
+            onChange={value => {
+              dispatch(updateResume({ is_public: false }));
+            }}
+          >
+            이력서를 비공개합니다.
+          </Editor.Checkbox>
+        </Editor.Cell>
       </Editor.Row>
     </Editor>
   );
