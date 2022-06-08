b48a3c4316345156a3b3afaec86868fb6f13a597
Avantgarde95 <mathematicianscott@gmail.com>
2022-06-08 21:01:00 +0900

    [chore] 채용공고 페이지에 alert 하나 추가
    * 서버 쪽에 patch가 없는 것 같아... 이게 확실하면 데모까지는 미구현으로
      유지

--- src/pages/company/job-editor.tsx
+++ src/pages/company/job-editor.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Wrapper from "@/components/wrapper";
 import Page from "@/components/page";
@@ -8,16 +8,22 @@ import SidebarColumn from "@/containers/company/job-editor/SidebarColumn";
 /**
  * 채용공고 작성 페이지.
  */
-const JobEditorPage = () => (
-  <Wrapper.Auth allowedAccountTypes={["company"]}>
-    <Page>
-      <Page.Title>채용공고 등록</Page.Title>
-      <Page.Columns>
-        <EditorColumn />
-        <SidebarColumn />
-      </Page.Columns>
-    </Page>
-  </Wrapper.Auth>
-);
+const JobEditorPage = () => {
+  useEffect(() => {
+    alert("채용공고는 아직 등록하기 버튼이 동작하지 않습니다. 빠른 시일 내에 구현하겠습니다.");
+  }, []);
+
+  return (
+    <Wrapper.Auth allowedAccountTypes={["company"]}>
+      <Page>
+        <Page.Title>채용공고 등록</Page.Title>
+        <Page.Columns>
+          <EditorColumn />
+          <SidebarColumn />
+        </Page.Columns>
+      </Page>
+    </Wrapper.Auth>
+  );
+};
 
 export default JobEditorPage;
