3eda7ba18dc6c8cf08818fa44b6b8f73f7ed3a84
Avantgarde95 <mathematicianscott@gmail.com>
2022-06-12 14:28:43 +0900

    [feat] 기업 정보 추가
    * 기업 정보 추가
    * 탭 만들기엔 시간이 많이 없어 일단 몰아넣음

--- src/pages/seeker/company-info.tsx
+++ src/pages/seeker/company-info.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Wrapper from "@/components/wrapper";
 import Page from "@/components/page";
 import BannerSection from "@/containers/seeker/company-info/BannerSection";
+import InfoSection from "@/containers/seeker/company-info/InfoSection";
 
 /**
  * 기업정보 페이지.
@@ -11,6 +12,7 @@ const CompanyInfoPage = () => (
   <Wrapper.Auth allowedAccountTypes={["seeker"]}>
     <Page>
       <BannerSection />
+      <InfoSection />
     </Page>
   </Wrapper.Auth>
 );
